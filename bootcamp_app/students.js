2f18457ff569a1cfa7dce29bf20d3d50916fadc5
Kazuko Baynton <kazukobaynton@gmail.com>
2022-04-14 18:48:47 -0700

    prevenst SQL injection change to parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -4,16 +4,21 @@ const pool = new Pool ({
   user: 'vagrant',
   passowrd: '123',
   host : 'localhost',
+  port:5432,
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+// store all potentially malicious values in an array
+const values = [`%${cohortName}%`, limit];
+
+pool.query(
+  `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,15 +11,18 @@ pool.connect(() => {
   console.log("Connected to the Database");
 });
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
